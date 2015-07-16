274e3968b4cb093e9021331d1d036e397f78cd8e
Justin Johnson <johnsonj561@gmail.com>
2015-07-15 22:03:09 -0400

    Glyphicons Added

--- index.php
+++ index.php
@@ -1,149 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>KB Software Solutions | West Palm Beach</title>
-
-    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
     <link href="css/small-business.css" rel="stylesheet">
+  </head>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
+  <body>
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand kb-logo" href="#"> 
-                     <img src="img/kb-logo-wide-400x80.png" alt="KB Software Solutions Logo"> 
-                </a> 
-                <a class="navbar-brand topnav kb-text" href="index.html">KB Software Solutions</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand kb-logo" href="#"> 
+            <img src="img/kb-logo-wide-400x80.png" alt="KB Software Solutions Logo"> 
+          </a> 
+          <a class="navbar-brand topnav kb-text" href="index.html">KB Software Solutions</a>
         </div>
-        <!-- /.container -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li>
+              <a href="view/about.php">About</a>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="view/services.php">Services<span class="caret"></span></a>
+              <ul class="dropdown-menu" role ="menu">
+                <li><a href="view/services.php#design">Design</a></li>
+                <hr class="drop-down-border">
+                <li><a href="view/services.php#maintenance">Maintenance</a></li>
+                <hr class="drop-down-border">
+                <li><a href="view/services.php#seo">SEO</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="view/contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Heading Row -->
-        <div class="row">
-            <div class="col-md-7">
-                <img class="img-responsive img-rounded" src="img/internet-logo-design.jpg" alt="">
-            </div>
-            <!-- /.col-md-8 -->
-            <div class="col-md-5">
-                <h1>Web Design, Maintenance, &amp; Performance Upgrades</h1>
-                <p>KB Software Solutions specializes in web development for small to medium sized businesses, 
-                  routine website maintenance, and SEO performance upgrades to existing web designs. KB Software Solutions 
-                  is independently owned and operated out of West Palm Beach, Florida.</p>
-                <p>97% of consumers search online before purchasing products and services. An
-                  effective, up to date website that is optimized to today's search engine standards is absolutely crucial 
-                  to any new or old business hoping to attract new customers.</p>
-                <p>See how KB Software Solutions can get your new business online or increase the visibility of your
-              already existing website</p>
-                <a class="btn btn-primary btn-lg" href="view/contact.php">Request Estimate</a>
-            </div>
-            <!-- /.col-md-4 -->
+    <div class="container" name="top">
+      <div class="row">
+        <div class="col-md-7">
+          <img class="img-responsive img-rounded" src="img/internet-logo-design.jpg" alt="Web development and design">
         </div>
-        <!-- /.row -->
-
-        
-      <hr>
-
-        <!-- Call to Action Well -->
-       <!-- <div class="row">
-            <div class="col-lg-12">
-                <div class="well text-center call-now">
-                    Call Now For Immediate Service 561-909-7795
-            </div>
-            <!-- /.col-lg-12 -->
-    <!--    </div> -->
-        <!-- /.row -->
-
-        <!-- Content Row -->
-        <div class="row">
-            <div class="col-md-4">
-                <h2 class="text-center">Web Design</h2>
-                <p>KB provides personalized web designs for you and your business at an affordable rate. Choose from 
-                  a variety of templates and design strategies that are proven successful, and let KB generate the 
-                  perfect website to launch your new business.</p>
-                <a class="btn btn-default" href="#">More Info</a>
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-                <h2 class="text-center">SEO Strategies</h2>
-                <p>Ever wonder what makes one web page appear before another when using Google or Yahoo? These search
-                  engines rely on strategic algorithms to produce the best possible results for consumers. Make 
-                  sure your business is reaching as many customers as possible and receive free Search Engine test results
-                  from KB Software Solutions. Increasing site design and applying various proven SEO strategies will 
-                  generate higher rankings and allow your name to reach more customers.</p>
-                <a class="btn btn-default" href="#">More Info</a>
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-                <h2 class="text-center">Web Maintenance</h2>
-                <p>A key to high site rankings and high visibility is fresh content and regular updates. Don't let
-              your site go stale, and see what KB can do to give your current site a fresh new look.</p>
-                <a class="btn btn-default" href="#">More Info</a>
-            </div>
-            <!-- /.col-md-4 -->
-        </div>
-        <!-- /.row -->
-      
-        <?php
-          require_once('template/footer.html');
-        ?>
-
+        <div class="col-md-5">
+          <h1>Web Design, Maintenance, &amp; Optimization</h1>
+          <p>KB Software Solutions specializes in web development for small to medium sized businesses, 
+            routine website maintenance, and SEO performance upgrades to existing designs.</p>
+          <p class="quote">"97% of consumers use the internet to research products or services in their local area"
+            <a href="http://www.biakelsey.com/" class="cite-source">BIA/Kelsey</a></p>
+          An effective, up to date website that is optimized to today's search engine 
+          standards is absolutely crucial to any new or old business hoping to attract 
+          new customers. See how KB Software Solutions can help get your business the results it's looking for.</p>
+        <a class="btn btn-primary btn-lg pull-right" href="view/contact.php">Request Estimate</a>
+      </div>
     </div>
-    <!-- /.container -->
+    <hr>
+    <div class="row">
+      <div class="col-md-4">
+        <h1 class="text-center"><span class="glyphicon glyphicon-list-alt glyph-box"></span></h1>
+        <h2 class="text-center">Web Design</h2>
+        <p>Providing personalized web designs for you and your business at an affordable rate. Choose from 
+          a variety of templates and let KB generate the perfect website to launch your new business.</p>
+        <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div>
+      </div>
+      <div class="col-md-4">
+        <h1 class="text-center"><span class="glyphicon glyphicon-stats glyph-box"></span></h1>
+        <h2 class="text-center">SEO Strategies</h2>
+        <p>Search engines rely on strategic algorithms to produce the best possible search results.
+          Improving site design and applying various proven SEO strategies will generate higher rankings and reach more
+        customers.</p>
+        <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div>
+      </div>
+      <div class="col-md-4">
+        <h1 class="text-center"><span class="glyphicon glyphicon-wrench glyph-box"></span></h1>
+        <h2 class="text-center">Web Maintenance</h2>
+        <p>A key to high site rankings and high visibility is fresh content and regular updates. Don't let
+          your site go stale - see what KB Software Solutions can do to give your current site a fresh new look.</p>
+        <div class="text-center"><a class="btn btn-default" href="#">More Info</a></div>
+      </div>
+    </div>
+    <hr>
+    <div class="row"><a href="#top">Back to Top</a><br></div>
+    </div>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="col-lg-6">
+            <ul class="list-inline">
+              <li>
+                <a href="#top">Home</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="view/about.php">About</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="view/services.php">Services</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="view/contact.php">Contact</a>
+              </li>
+            </ul>
+            <p class="copyright text-muted small">Copyright &copy; KB Software Solutions 2015. All Rights Reserved</p>
+          </div>
+          <div class="col-lg-6">
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
+            <p class="copyright text-muted medium pull-right">
+              <img href="www.kbsoftwaresolutions.com" src="img/kb-logo-v3-75x60.png" alt="KB Software Solutions Logo">
+              <a href="www.kbsoftwaresolutions.com">Website Development by KB Software Solutions</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- jQuery -->
+  <script src="js/jquery.js"></script>
+  <!-- Bootstrap Core JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
