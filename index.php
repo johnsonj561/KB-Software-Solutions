8b0ecb5bbf6cfd2ed4592e5a9366ce57fbfe360d
Justin Johnson <johnsonj561@gmail.com>
2015-07-22 20:09:24 -0400

    Content Updated

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="KB Software Solutions - Providing small to medium
+                                      sized businesses with affordable web designs and maintenance.">
+    <meta name="author" content="KB Software Solutions">
+    <meta name="ROBOTS" content="INDEX, FOLLOW">
+    <link rel="icon" type="image/x-icon" href="img/kb-favicon.png" />
     <title>KB Software Solutions | West Palm Beach</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/small-business.css" rel="stylesheet">
@@ -22,23 +25,30 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand kb-logo" href="#"> 
-            <img src="img/kb-logo-wide-400x80.png" alt="KB Software Solutions Logo"> 
+            <img src="img/kb-logo-wide-380x70.png" alt="KB Software Solutions Logo"> 
           </a> 
-          <a class="navbar-brand topnav kb-text" href="index.html">KB Software Solutions</a>
+          <a class="navbar-brand topnav kb-text" href="index.php">KB Software Solutions</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li>
               <a href="view/about.php">About</a>
             </li>
+            <li class="mobile-only">
+              <a href="view/services.php">Services</a>
+            </li>
             <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="view/services.php">Services<span class="caret"></span></a>
               <ul class="dropdown-menu" role ="menu">
-                <li><a href="view/services.php#design">Design</a></li>
+                <li><a href="view/services.php">Design</a></li>
                 <hr class="drop-down-border">
                 <li><a href="view/services.php#maintenance">Maintenance</a></li>
                 <hr class="drop-down-border">
                 <li><a href="view/services.php#seo">SEO</a></li>
+                <hr class="drop-down-border">
+                <li><a href="view/services.php#database-management">Database Tools</a></li>
+                <hr class="drop-down-border">
+                <li><a href="view/services.php#social-media">Social Media</a></li>
               </ul>
             </li>
             <li>
@@ -51,86 +61,126 @@
     <div class="container" name="top">
       <div class="row">
         <div class="col-md-7">
-          <img class="img-responsive img-rounded" src="img/internet-logo-design.jpg" alt="Web development and design">
+          <img class="img-responsive img-rounded wrap-image" src="img/internet-logo-design.jpg" alt="Web development and design">
         </div>
         <div class="col-md-5">
-          <h1>Web Design, Maintenance, &amp; Optimization</h1>
-          <p>KB Software Solutions specializes in web development for small to medium sized businesses, 
-            routine website maintenance, and SEO performance upgrades to existing designs.</p>
-          <p class="quote">"97% of consumers use the internet to research products or services in their local area"
+          <h1>Small Business Web Design</h1>
+          <p>KB Software Solutions delivers custom web designs to local businesses for affordable
+            costs. Get your new business online or restore an existing design and attract new customers.</p>
+          <p class="quote">"97% of consumers use the internet to research products or services in their 
+            local area"
             <a href="http://www.biakelsey.com/" class="cite-source">BIA/Kelsey</a></p>
-          An effective, up to date website that is optimized to today's search engine 
-          standards is absolutely crucial to any new or old business hoping to attract 
-          new customers. See how KB Software Solutions can help get your business the results it's looking for.</p>
-        <a class="btn btn-primary btn-lg pull-right" href="view/contact.php">Request Estimate</a>
+          <p>An effective, up to date website that is both aesthetically pleasing and optimized to today's 
+            search engine standards is crucial to any new or old business attracting
+            new customers. See how KB Software Solutions can help get your business the results it's 
+            looking for.</p>
+          <a class="btn btn-primary btn-lg pull-right" href="view/contact.php">Request Estimate</a>
+        </div>
       </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="col-md-4">
-        <h1 class="text-center"><span class="glyphicon glyphicon-list-alt glyph-box"></span></h1>
-        <h2 class="text-center">Web Design</h2>
-        <p>Providing personalized web designs for you and your business at an affordable rate. Choose from 
-          a variety of templates and let KB generate the perfect website to launch your new business.</p>
-        <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div>
-      </div>
-      <div class="col-md-4">
-        <h1 class="text-center"><span class="glyphicon glyphicon-stats glyph-box"></span></h1>
-        <h2 class="text-center">SEO Strategies</h2>
-        <p>Search engines rely on strategic algorithms to produce the best possible search results.
-          Improving site design and applying various proven SEO strategies will generate higher rankings and reach more
-        customers.</p>
-        <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div>
-      </div>
-      <div class="col-md-4">
-        <h1 class="text-center"><span class="glyphicon glyphicon-wrench glyph-box"></span></h1>
-        <h2 class="text-center">Web Maintenance</h2>
-        <p>A key to high site rankings and high visibility is fresh content and regular updates. Don't let
-          your site go stale - see what KB Software Solutions can do to give your current site a fresh new look.</p>
-        <div class="text-center"><a class="btn btn-default" href="#">More Info</a></div>
-      </div>
-    </div>
-    <hr>
-    <div class="row"><a href="#top">Back to Top</a><br></div>
-    </div>
-  <footer>
-    <div class="container">
+      <hr>
       <div class="row">
-        <div class="col-lg-12">
-          <div class="col-lg-6">
-            <ul class="list-inline">
-              <li>
-                <a href="#top">Home</a>
-              </li>
-              <li class="footer-menu-divider">&sdot;</li>
-              <li>
-                <a href="view/about.php">About</a>
-              </li>
-              <li class="footer-menu-divider">&sdot;</li>
-              <li>
-                <a href="view/services.php">Services</a>
-              </li>
-              <li class="footer-menu-divider">&sdot;</li>
-              <li>
-                <a href="view/contact.php">Contact</a>
-              </li>
-            </ul>
-            <p class="copyright text-muted small">Copyright &copy; KB Software Solutions 2015. All Rights Reserved</p>
-          </div>
-          <div class="col-lg-6">
+        <div class="col-md-4">
+          <a href="view/services.php"><h1 class="text-center">
+            <span class="glyphicon glyphicon-list-alt glyph-box"></span></h1></a>
+          <h2 class="text-center">Web Design</h2>
+          <p>Providing personalized web designs for you and your business at an affordable rate. Choose from 
+            a variety of templates and let KB generate the perfect website to launch your new business.</p>
+          <!--      <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div> -->
+        </div>
+        <div class="col-md-4">
+          <a href="view/services.php#seo"><h1 class="text-center">
+            <span class="glyphicon glyphicon-stats glyph-box"></span></h1></a>
+          <h2 class="text-center">SEO Strategies</h2>
+          <p>Search engines rely on strategic algorithms to produce the best possible search results.
+            Improving site design and applying various proven SEO strategies will generate higher rankings and reach more
+            customers.</p>
+          <!--   <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div> -->
+        </div>
+        <div class="col-md-4">
+          <a href="view/services.php#maintenance"><h1 class="text-center">
+            <span class="glyphicon glyphicon-wrench glyph-box"></span></h1></a>
+          <h2 class="text-center">Web Maintenance</h2>
+          <p>Routine maintenance is needed to keep up with changes to products and services, advertise promotions, upload new
+            content, and keep your site running efficiently. Don't let
+            your site go stale - see what KB Software Solutions can do to give your current site a fresh new look.</p>
+          <!--  <div class="text-center"><a class="btn btn-default" href="#">More Info</a></div> -->
+        </div>
+      </div>
+      <hr class="paragraph-divider">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+          <a href="view/services.php#database-management"><h1 class="text-center">
+            <span class="glyphicon glyphicon-floppy-disk glyph-box"></span></h1></a>
+          <h2 class="text-center">Database Management</h2>
+          <p>Database tools allow it's users to search and operate on thousands of records in fractions of a second. 
+            Whether it be customer information, product details, or sales records - letting KB Software Solutions develop a custom 
+            database interface can save you crucial time and money.</p>
+          <!--      <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div> -->
+        </div>
+        <div class="col-md-4 pull-left">
+          <a href="view/services.php#social-media"><h1 class="text-center">
+            <span class="glyphicon glyphicon-share glyph-box"></span></h1></a>
+          <h2 class="text-center">Social Media</h2>
+          <p>Stay connected and take advantage of the free advertising available on Facebook, Twitter, Pinterest, Google+, and
+            other social media platforms. Increased social media activity, when incorporated into your web design, will increase
+            search rankings and generate more business.</p>
+          <!--   <div class="text-center"><a class="btn btn-default text-center" href="#">More Info</a></div> -->
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+      <hr class="paragraph-divider">
+      <div class="row"><a href="#top">Back to Top</a><br></div>
+    </div>
+    <row class="text-center social-media mobile-only">
+      <ul class="share-buttons">
+        <li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.kbsoftwaresolutions.com&t=" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;"><img src="img/Facebook.png"></a></li>
+        <li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fwww.kbsoftwaresolutions.com&text=:%20http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ':%20'  + encodeURIComponent(document.URL)); return false;"><img src="img/Twitter.png"></a></li>
+        <li><a href="https://plus.google.com/share?url=http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" title="Share on Google+" onclick="window.open('https://plus.google.com/share?url=' + encodeURIComponent(document.URL)); return false;"><img src="img/Google+.png"></a></li>
+        <li><a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.kbsoftwaresolutions.com&description=" target="_blank" title="Pin it" onclick="window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(document.URL) + '&description=' +  encodeURIComponent(document.title)); return false;"><img src="img/Pinterest.png"></a></li>
+        <li><a href="mailto:?subject=&body=:%20http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" title="Email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img src="img/Email.png"></a></li>
+      </ul>
+    </row>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="col-lg-6">
+              <ul class="list-inline">
+                <li>
+                  <a href="#top">Home</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                  <a href="view/about.php">About</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                  <a href="view/services.php">Services</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                  <a href="view/contact.php">Contact</a>
+                </li>
+              </ul>
+              <p class="copyright text-muted small">Copyright &copy; KB Software Solutions 2015. All Rights Reserved</p>
+            </div>
+            <div class="col-lg-6">
 
-            <p class="copyright text-muted medium pull-right">
-              <img href="www.kbsoftwaresolutions.com" src="img/kb-logo-v3-75x60.png" alt="KB Software Solutions Logo">
-              <a href="www.kbsoftwaresolutions.com">Website Development by KB Software Solutions</a>
-            </p>
+              <p class="copyright text-muted medium pull-right">
+                <img href="www.kbsoftwaresolutions.com" src="img/kb-logo-v3-75x60.png" alt="KB Software Solutions Logo">
+                <a href="www.kbsoftwaresolutions.com">Website Development by KB Software Solutions</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
-  <!-- jQuery -->
-  <script src="js/jquery.js"></script>
-  <!-- Bootstrap Core JavaScript -->
-  <script src="js/bootstrap.min.js"></script>
+    </footer>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/slide-toggle-nav.js"></script>
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55aed0f95bca4219" async="async"></script>
   </body>
 </html>
