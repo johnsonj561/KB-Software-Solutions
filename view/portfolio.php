a601e6a2fcd90d0283a91f6d66bd59fad621f067
Justin Johnson <johnsonj561@gmail.com>
2015-09-02 09:08:07 -0400

    1st Commit

---
+++ view/portfolio.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="KB Software Solutions - Providing small businesses with custom web designs for affordable costs.">
+    <meta name="author" content="KB Software Solutions">
+    <meta name="ROBOTS" content="INDEX, FOLLOW">
+    <link rel="icon" type="image/x-icon" href="../img/kb-favicon.png" />
+    <title>KB Software Solutions | About</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/kb-css.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <?php require_once('../template/navbar.html'); ?>
+    <div class="container"> 
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header text-center">Website Designs by KB Software Solutions
+          </h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-responsive wrap-image" src="../img/700x300/sharpcutlawns-homepage-700x325.jpg" alt="Sharpcut Lawn Service Home Page">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>Sharpcut Lawn Service, Branchburg NJ</h3>
+          <ul>
+            <li>Responsive design provides clean layouts on PCs, Tablets, and Mobile Devices</li>
+            <li>Informative home page that summarizes business services and displays slideshow of showcased work</li>
+            <li>Contact page that provides business information and allows users to message Sharp Cut Lawn Service</li>
+            <li>Account management lets customers log in to view work orders and make payments</li>
+            <li>Administrative features to upload educational articles, provide customers with work orders, and track payments</li>
+          </ul>
+          <a class="btn btn-primary" href="http://www.sharpcutlawns.com" target="_blank">Visit Website</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-responsive wrap-image" src="../img/700x300/cntautocare-homepage-700x325.jpg" alt="C&T Auto Care Home Page">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>C &amp; T Auto Care, Singer Island FL</h3>
+          <ul>
+            <li>Clean, crisp, modern design provides customers with an easy to navigate and informative website</li>
+            <li>Responsive design automatically reformats layouts when viewed from mobile devices</li>
+            <li>Interactive Google Map is embedded into home page, providing customers with quick directions to auto shop</li>
+            <li>Social Media connectons encourage customer reviews, boosting SEO rankings and increasing visibility</li>
+          </ul>
+          <a class="btn btn-primary" href="http://www.cntautocare.com" target="_blank">Visit Website</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-responsive wrap-image" src="../img/700x300/archrecovery-homepage-700x325.jpg" alt="ARCH Florida Recovery Houses">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>ARCH Florida Housing, West Palm Beach</h3>
+          <ul>
+            <li>Aesthetic design with large background image to capture the attention of potential customers</li>
+            <li>Professional images purchased from <a href="http://www.shutterstock.com" target="_blank">Shutterstock</a> enhance
+            overall design and appearance of website</li>
+            <li>Photo gallery with an interactive slide-show to effectively advertise properties and primary features</li>
+            <li>Social media marketing links website to Facebook, Twitter, and Google</li>
+            <li>Additional pages educate customers, encourage website interaction, and increase SEO scores with quality content</li>
+          </ul>
+          <a class="btn btn-primary" href="http://www.archrecoveryhouses.com" target="_blank">Visit Website</a>
+        </div>
+      </div>
+
+      <hr>
+
+      <!-- Pagination 
+      <div class="row text-center">
+        <div class="col-lg-12">
+          <ul class="pagination">
+            <li>
+              <a href="#">&laquo;</a>
+            </li>
+            <li class="active">
+              <a href="#">1</a>
+            </li>
+            <li>
+              <a href="#">2</a>
+            </li>
+            <li>
+              <a href="#">3</a>
+            </li>
+            <li>
+              <a href="#">4</a>
+            </li>
+            <li>
+              <a href="#">5</a>
+            </li>
+            <li>
+              <a href="#">&raquo;</a>
+            </li>
+          </ul>
+        </div>
+      </div>  -->
+      
+      <div class="row"><a href="#top">Back to Top</a><br></div>
+    </div> 
+    <?php require_once('../template/social-media.html');?>
+    <?php require_once('../template/footer.html'); ?>
+  </body>
+</html>
